8e4215030d33cbef24f5c12c6f895e680ae989b7
João Neto <jflorencioadv@gmail.com>
2023-12-16 14:33:19 +0100

    Developflix

--- js/script.js
+++ js/script.js
@@ -1,33 +1,36 @@
 import peliculas from './peliculas.js'
+console.log(peliculas)
 
-const generos = {
-    28: 'Ação',
-    12: 'Aventura',
-    53: 'Thriller',
+
+function mostrarPeliculasPorGenero(generoId, contenedorId) {
+    const contenedor = document.getElementById(contenedorId);
+    const peliculasGenero = peliculas.filter((pelicula) => pelicula.genre_ids.includes(generoId));
+
+    peliculasGenero.forEach((pelicula) => {
+        const peliculaDiv = document.createElement("div");
+        peliculaDiv.classList.add("pelicula");
+
+        const titulo = document.createElement("p");
+        titulo.classList.add("titulo");
+        titulo.textContent = pelicula.title;
+
+        const poster = document.createElement("img");
+        poster.classList.add("poster");
+        poster.src = `https://image.tmdb.org/t/p/w200${pelicula.poster_path}`;
+        poster.alt = pelicula.title;
+
+        peliculaDiv.appendChild(poster);
+        peliculaDiv.appendChild(titulo);
+
+        contenedor.appendChild(peliculaDiv);
+    });
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    const container = document.querySelector('.movie-container');
-  
-    Object.keys(generos).forEach(generoId => {
-      const filmesGenero = peliculas.filter(pelicula => pelicula.genre_ids.includes(parseInt(generoId)));
-      
-      if (filmesGenero.length > 0) {
-        const generoDiv = document.createElement('div');
-        generoDiv.className = 'genero';
-        generoDiv.innerHTML = `<h2>${generos[generoId]}</h2>`;
-  
-        filmesGenero.forEach(pelicula => {
-          const movieDiv = document.createElement('div');
-          movieDiv.className = 'movie';
-          movieDiv.innerHTML = `
-            <h3>${pelicula.title}</h3>
-            <img src="https://image.tmdb.org/t/p/w500${pelicula.poster_path}" alt="${pelicula.title}">
-          `;
-          generoDiv.appendChild(movieDiv);
-        });
-  
-        container.appendChild(generoDiv);
-      }
-    });
-  });
+// Mostrar películas de acción (Género 28)
+mostrarPeliculasPorGenero(28, "genero-28");
+
+// Mostrar películas de thriller (Género 53)
+mostrarPeliculasPorGenero(53, "genero-53");
+
+// Mostrar películas de aventura (Género 12)
+mostrarPeliculasPorGenero(12, "genero-12");
